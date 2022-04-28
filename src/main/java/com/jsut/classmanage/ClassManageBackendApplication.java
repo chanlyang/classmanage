4523051dca4423cf4dd896eac35e6a29965900fc
chenliang <ygglyang@gmail.com>
2022-04-28 21:17:23 +0800

    Initial commit

---
+++ src/main/java/com/jsut/classmanage/ClassManageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jsut.classmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassManageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassManageBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jsut/classmanage/ClassManageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsut.classmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
